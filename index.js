08d454e62462985ef0b6c07281bc4031c898b882
Joseph <joffan98@gmail.com>
2022-09-19 16:40:36 -0500

    commit tree f4d91ce595f81d3c39bcfccee171aadb631784d9

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42)
+}
+
+function distanceFromHqInFeet(someValue) {
+    const distanceFeet = distanceFromHqInBlocks(someValue) * 264
+    return distanceFeet
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const distanceBlocks = Math.abs(destination - start)
+    return distanceBlocks * 264
+}
+
+function  calculatesFarePrice(start, destination) {
+    const feet = distanceTravelledInFeet(start, destination)
+    if (feet <= 400) return 0
+    else if (feet <= 2000) return (feet - 400) * 0.02 
+    else if (feet <= 2500) return 25
+    else if (feet > 2500) return "cannot travel that far"
+    else return "404 error something went wrong"
+}
